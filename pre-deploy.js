0c0252ac31749460350ec8a8a37ad1bcb62953a9
YangyangLi <liyangyang@authing.cn>
2024-12-31 16:49:30 +0800

    fix statistics

--- pre-deploy.js
+++ pre-deploy.js
@@ -4,5 +4,5 @@ const fs = require('fs');
 const _config = yaml.parse(fs.readFileSync('_config.fluid.yml', 'utf8'));
 _config.gitalk.clientID = process.env.OAUTH_GITHUB_CLIENT_ID;
 _config.gitalk.clientSecret = process.env.OAUTH_GITHUB_SECRET;
-_config.web_analytics.baidu.server_url = process.env.BAIDU;
+_config.web_analytics.baidu = process.env.BAIDU;
 fs.writeFileSync('_config.fluid.yml', yaml.stringify(_config));
