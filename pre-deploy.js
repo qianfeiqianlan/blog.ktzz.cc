bd9aa757de5991a8a45d142f2341d250b5fefd8a
YangyangLi <liyangyang@authing.cn>
2024-12-31 16:38:57 +0800

    fix statistics

--- pre-deploy.js
+++ pre-deploy.js
@@ -4,7 +4,5 @@ const fs = require('fs');
 const _config = yaml.parse(fs.readFileSync('_config.fluid.yml', 'utf8'));
 _config.gitalk.clientID = process.env.OAUTH_GITHUB_CLIENT_ID;
 _config.gitalk.clientSecret = process.env.OAUTH_GITHUB_SECRET;
-_config.web_analytics.leancloud.app_id = process.env.LEANCLOUD_APP_ID;
-_config.web_analytics.leancloud.app_key = process.env.LEANCLOUD_APP_KEY;
-_config.web_analytics.leancloud.server_url = process.env.LEANCLOUD_SERVER_URL;
+_config.web_analytics.baidu.server_url = process.env.BAIDU;
 fs.writeFileSync('_config.fluid.yml', yaml.stringify(_config));
